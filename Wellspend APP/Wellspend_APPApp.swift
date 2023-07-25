97c48a9341881a9d1bfaff538949370d9c29d8ff
Dennis Bär <Dennis.baer@Student.Reutlingen-university.de>
2023-07-25 19:01:26 +0200

    Initial Commit

---
+++ Wellspend APP/Wellspend_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wellspend_APPApp.swift
+//  Wellspend APP
+//
+//  Created by Dennis Bär on 25.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Wellspend_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
